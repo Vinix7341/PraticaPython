6875c6d7dc7f60cf8b94f88d210a66743ef9bf54
Vinícius Xavier <vinix1508@gmail.com>
2023-08-01 11:14:10 -0300

    ver.2.0. O ex_fixacao02.py completo.

---
+++ ex_fixacao02.py
@@ -0,0 +1,5 @@
+val = float(input("Digite o valor da conta do restaurante que eu irei lhe dizer o valor do acréscimo do garçom: "))
+
+tot = val + val / 10
+
+print("O total da conta com o acréscimo do garçom é", tot)
